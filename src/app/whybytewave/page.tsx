f74de045e562d019f1bf532e427d4033fa46098f
Lyondealpha-hu <AlhajiAhmed400@gmail.com>
2023-06-11 22:49:27 +0000

    jun11 closing

---
+++ src/app/whybytewave/page.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+type Props = {}
+
+export default function WhyBytewave({}: Props) {
+  return (
+    <div className='w-full h-full bg-white py-3 '>
+      <div className='w-full justify-center items-center lg:px-8'>
+        <div className="relative max-w-5xl mx-auto pt-10 sm:pt-10 lg:pt-10 justify-center items-center text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600 italic">Bytewave</h2>
+          <h1 className="text-slate-900 font-bold text-4xl sm:text-5xl lg:text-5xl tracking-tight text-center dark:text-white text-black uppercase" >
+              Building a perfect solution according to your business goals
+          </h1>
+          <p className="mt-6 text-lg text-slate-600 text-center text-gray-600 max-w-5xl mx-auto dark:text-slate-400">
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+              Driven by experienced teams and an agile framework, we prioritize our 
+              clients' commercial goals to deliver exceptional business value.  
+          </p>
+        </div>
+      </div>
+    
+    </div>
+  )
+}
