cfd84ff6950e60ad9cb43af46b60b14905c70db7
Lyondealpha-hu <AlhajiAhmed400@gmail.com>
2023-09-30 10:30:51 +0000

    changes

--- src/pages/components/templates/textArea_template.tsx
+++ src/pages/components/templates/textArea_template.tsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react'
 import { Form, Input } from 'antd';
-import TextArea from 'antd/es/input/TextArea';
-import { kMaxLength } from 'buffer';
 
 
 interface props {
-    label: string;
-    inputType: string;
-    placeholder: string;
-    isAllowClear: boolean;
-    isRequired: boolean;
-    isShowCount: boolean;
-    maxLength: number;
-    requiredPrompt: string;
-    height: string | number;
+    label?: string;
+    inputType?: string;
+    placeholder?: string;
+    isAllowClear?: boolean;
+    isRequired?: boolean;
+    isShowCount?: boolean;
+    maxLength?: number;
+    requiredPrompt?: string;
+    height?: string | number;
     labelCol?: number
     wrapperCol?: number
     textareaStyle?: any
@@ -24,6 +22,7 @@ interface props {
 
 export const TextArea_template = ({ label, textareaStyle, inputType, placeholder, setInputValue, isAllowClear, isRequired, isShowCount, maxLength, requiredPrompt, height, labelCol, wrapperCol }: props) => {
     const [data, setData] = useState<string>()
+    const { TextArea } = Input;
     return (
 
         <div>
